decec6edadd42aa5e4f7cfa8487835a8fb180cfc
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-08 15:03:59 +0000

    Pass the cardType as a top level prop

--- html/render.js
+++ html/render.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import HomePagePanel from '../src/index.js'
+import AtlasHPCard from 'atlas-homepage-cards'
 
 const render = (options, target) => {
-  ReactDOM.render(<HomePagePanel {...options} />, document.getElementById(target))
+  ReactDOM.render(<HomePagePanel {...options} cardType={AtlasHPCard} />, document.getElementById(target))
 }
 
 export {render}
