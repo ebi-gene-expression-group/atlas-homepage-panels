013f85f9c14080ff829170f9d20f6c3a7eb9f24b
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-08 15:04:31 +0000

    Remove useless divs in small featured card

--- src/Card.js
+++ src/Card.js
@@ -2,16 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import logo from './assets/img/HCA-logo.jpg'
 
-const Card = ({host, cardName}) => {
+const Card = ({host, resource, cardName}) => {
   	return (
 		<div className="small-4 columns combo text-center padding-top-medium">
-	  		<span>I am a card for {cardName}</span> <br />
-	        <a href={`${host}/${cardName.toLowerCase()}`}>
+	        <a href={`${host}${resource}`}>
 	            <img className="margin-bottom-large experiment-thumbnail" src={logo}  height="800" />
-	            <h6>
-	                <a href="/gxa/experiments/E-MTAB-4344">Human tissues</a><br />
-	                <a href="/gxa/experiments/E-GEOD-26284">Human cells</a>
-	            </h6>
 	        </a>
 		</div>
   	)
