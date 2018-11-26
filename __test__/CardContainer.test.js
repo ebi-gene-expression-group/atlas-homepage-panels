40e1113e12fe62ce8c24f27d2ba04e813b5f3a65
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-26 11:29:55 +0000

    Update tests and snapshots

--- __test__/CardContainer.test.js
+++ __test__/CardContainer.test.js
@@ -1,37 +1,34 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Enzyme from 'enzyme'
-import {shallow, mount, render} from 'enzyme'
+import { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 import CardContainer from '../src/CardContainer'
 import LatestExperimentCardContainer from '../src/LatestExperimentCardContainer'
 import Card from '../src/Card'
-import {panelName, resource, host, CardType} from './TestUtils'
+import {panelName, resource, host} from './TestUtils'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe(`CardContainer`, () => {
-  const props = {host, resource, panelName, CardType}
+  const props = {host, resource, panelName}
   test(`uses a LatestExperimentCard for latest experiment entries`, () => {
-  	 const wrapper = shallow(<CardContainer {...props} panelName={`Latest experiments`}/>)
-     expect(wrapper.find(LatestExperimentCardContainer).length).toEqual(1)
-     expect(wrapper.find(Card).length).toEqual(0)
-     expect(wrapper.find(props.CardType).length).toEqual(0)
+  	const wrapper = shallow(<CardContainer {...props} panelName={`Latest experiments`}/>)
+    expect(wrapper.find(LatestExperimentCardContainer).length).toEqual(1)
+    expect(wrapper.find(Card).length).toEqual(0)
   })
 
   test(`uses a static card for featured experiment`, () => {
-  	 const wrapper = shallow(<CardContainer {...props} panelName={`Featured`}/>)
-     expect(wrapper.find(LatestExperimentCardContainer).length).toEqual(0)
-     expect(wrapper.find(Card).length).toEqual(1)
-     expect(wrapper.find(props.CardType).length).toEqual(0)
+  	const wrapper = shallow(<CardContainer {...props} panelName={`Featured`}/>)
+    expect(wrapper.find(LatestExperimentCardContainer).length).toEqual(0)
+    expect(wrapper.find(Card).length).toEqual(1)
   })
 
-  test(`uses a default/prop card for species experiment`, () => {
-  	 const wrapper = shallow(<CardContainer {...props} />)
-     expect(wrapper.find(LatestExperimentCardContainer).length).toEqual(0)
-     expect(wrapper.find(Card).length).toEqual(0)
-     expect(wrapper.find(props.CardType).length).toEqual(1)
+  test(`returns null if the tab name is not defined as default`, () => {
+  	const wrapper = shallow(<CardContainer {...props}/>)
+    expect(wrapper.find(LatestExperimentCardContainer).length).toEqual(0)
+    expect(wrapper.find(Card).length).toEqual(0)
   })
 
   test(`matches snapshot`, () => {
--- __test__/HomePagePanel.test.js
+++ __test__/HomePagePanel.test.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Enzyme from 'enzyme'
-import {shallow, mount, render} from 'enzyme'
+import { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
 import HomePagePanel from '../src/HomePagePanel'
 import PanelBar from '../src/PanelBar'
 import CardContainer from '../src/CardContainer'
-import {panelNameAList, panelNameBList, resource, host, CardType} from './TestUtils'
+
+import { panelNameAList, panelNameBList, resource, host, CardType } from './TestUtils'
 
 Enzyme.configure({ adapter: new Adapter() })
 
 describe(`HomePagePanel`, () => {
   const props = {panelNameAList, panelNameBList, resource, host, CardType}
-  test(`should have two panel boxes`, () => {
-     const wrapper = shallow(<HomePagePanel {...props}/>)
-     expect(wrapper.find(PanelBar).length).toEqual(2)
-     expect(wrapper.find(CardContainer).length).toEqual(2)
+  test(`should have two panel boxes, one experiment container, one species container`, () => {
+    const wrapper = shallow(<HomePagePanel {...props} />)
+    expect(wrapper.find(PanelBar).length).toEqual(2)
+    expect(wrapper.find(CardContainer).length).toEqual(1)
+    expect(wrapper.find(CardType).length).toEqual(1)
   })
 
   test(`renders different panel page after clicking each panel bar`, () => {
