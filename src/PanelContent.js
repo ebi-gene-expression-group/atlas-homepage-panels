762990c2faf7b6ccc9126e26205b402b07f257d9
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-30 15:41:30 +0000

    Reformat PanelContent codes

--- src/PanelContent.js
+++ src/PanelContent.js
@@ -2,26 +2,22 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import HCAlogo from './assets/img/HCA-logo.jpg'
 
-const PanelContent = ({panelName, host, resource, CardType}) => {
-  switch (panelName) {
-    case `Featured`:
-      return (    
-        <div className="small-4 columns combo text-center padding-top-medium">
-          <a href={`${host}${resource[panelName]}`}>
-              <img className="margin-bottom-large" src={HCAlogo}  height="400" />
-          </a>
-        </div>
-      )
-      break
-    default:
-      return <CardType host={host} resource={resource[panelName]}/>
-  } 
-}
+const PanelContent = ({panelName, host, resource, CardType}) => 
+  panelName === `Featured` ?     
+    <div className="small-4 columns combo text-center padding-top-medium">
+      <a href={`${host}${resource[panelName]}`}>
+          <img className="margin-bottom-large" src={HCAlogo}  height="400" />
+      </a>
+    </div>
+    :
+    <CardType host={host} resource={resource[panelName]}/>
+
 
 PanelContent.propTypes = {
-  panelName: PropTypes.string.isRequired,
-  host: PropTypes.string.isRequired,
-  resource: PropTypes.object.isRequired
+  panelName: PropTypes.string.isReq,
+  host: PropTypes.string,
+  resource: PropTypes.object,
+  CardType: PropTypes.func
 }
 
 export default PanelContent
