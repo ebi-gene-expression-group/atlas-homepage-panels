a8fc6e5ea048adeb411facc099489648c3c94338
lingyun1010 <zhaolingyun1010@gmail.com>
2018-11-30 13:09:34 +0000

    Import two containers from atlas-homepage-cards

--- html/render.js
+++ html/render.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import HomePagePanel from '../src/index.js'
-import { SceaHomepageSpeciesContainer } from 'atlas-homepage-cards'
+import { SceaHomepageSpeciesContainer, SceaHomePageLatestExperimentContainer } from 'atlas-homepage-cards'
 
 const render = (options, target) => {
-  ReactDOM.render(<HomePagePanel {...options} CardType={SceaHomepageSpeciesContainer} />, document.getElementById(target))
+  ReactDOM.render(<HomePagePanel {...options} SpeciesCardType={SceaHomepageSpeciesContainer} 
+    LatestExperimentCardType={SceaHomePageLatestExperimentContainer} />, document.getElementById(target))
 }
 
 export {render}
