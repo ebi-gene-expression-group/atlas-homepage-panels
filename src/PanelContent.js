e940a522e0276c0b2cda0ef1f11c99be01bdfed9
lingyun1010 <zhaolingyun1010@gmail.com>
2018-12-11 16:43:26 +0000

    Replace HCA logo with host resource

--- src/PanelContent.js
+++ src/PanelContent.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import HCAlogo from './assets/img/HCA-logo.jpg'
 
 const PanelContent = ({panelName, host, resource, CardType}) => 
   panelName === `Featured` ?     
     <div className={`small-4 columns combo text-center padding-top-medium`}>
       <a href={`${host}${resource[panelName]}`}>
-          <img className={`margin-bottom-large`} src={HCAlogo}  height={`400`} />
+          <img className={`margin-bottom-large`} src={`${host}/resources/images/logos/human_cell_atlas.png`}  height={`400`} />
       </a>
     </div>
     :
